c1fa185d395196877213e54eddfbe8d46de5d3e4
Mieko <miekotominaga@gmail.com>
2023-11-01 17:21:55 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,43 @@
-import './App.css';
+import "./App.css";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from "./utils/data";
+import { Bar } from "react-chartjs-2";
+
+Chart.register(CategoryScale);
 
 function App() {
-  return <div className='App'></div>;
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.drink),
+    datasets: [
+      {
+        data: Data.map((data) => data.value),
+        backgroundColor: ["brown", "blue"],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  return (
+    <div className="App">
+      <Bar
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Hydration",
+            },
+            legend: {
+              display: false,
+            },
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/utils/data.js
@@ -0,0 +1,12 @@
+export const Data = [
+  {
+    id: 1,
+    drink: "coffee",
+    value: 10,
+  },
+  {
+    id: 2,
+    drink: "water",
+    value: 3,
+  },
+];
