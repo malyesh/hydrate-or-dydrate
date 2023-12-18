a35dc01af030fca5a3f7d3e96a7f27de530914bf
malyesh <74512928+malyesh@users.noreply.github.com>
2023-12-17 22:01:13 -0500

    get user info for current day hydration levels

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-require("dotenv").config();
-const dataRoute = require("./routes/hydration");
-const authRoute = require("./routes/user-auth");
+require('dotenv').config();
+const dataRoute = require('./routes/hydration');
+const authRoute = require('./routes/user-auth');
 
-const origin = "http://localhost:3000";
+const origin = 'http://localhost:3000';
 
 app.use(
   cors({
@@ -14,17 +14,16 @@ app.use(
   })
 );
 
-app.use(express.static("public"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  console.log("Route endpoint");
-  res.send("Welcome to Hydrate or Dydrate");
+app.get('/', (req, res) => {
+  console.log('Route endpoint');
+  res.send('Welcome to Hydrate or Dydrate');
 });
 
-app.use("/auth", authRoute);
-app.use("/hydration", dataRoute);
+app.use('/auth', authRoute);
+app.use('/hydration', dataRoute);
 
 app.listen(8080, function () {
-  console.log("running on port 8080");
+  console.log('running on port 8080');
 });
